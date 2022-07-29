8bcdc369d31f554f6b9d37c19732fea78d85a92b
JoErnesto <jernesto.calvillo@gmail.com>
2022-07-28 17:38:47 -0700

    Modificación con mis datos
    Se modificó y agregó los datos para conectarse a mi red.

--- ESP32CAM-WiFi-Basic/ESP32CAM-WiFi-Basic.ino
+++ ESP32CAM-WiFi-Basic/ESP32CAM-WiFi-Basic.ino
@@ -21,8 +21,8 @@
 #include <WiFi.h>  // Biblioteca para manejar el WiFi del ESP32CAM
 
 // Datos de Red
-const char* ssid = "********";  // Pon aquí el nombre de la red a la que deseas conectarte
-const char* password = "********";  // Escribe la contraseña de dicha red
+const char* ssid = "INFINITUM29A4_2.4";  // Pon aquí el nombre de la red a la que deseas conectarte
+const char* password = "2gPXRJ28bg";  // Escribe la contraseña de dicha red
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja las variables necesarias para una conexion WiFi
